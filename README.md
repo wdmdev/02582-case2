1b942cdecbc50c0d2966532daa90c70bdef47b3c
William Marstrand <williammarstrand@Williams-MacBook-Pro.local>
2022-04-18 07:54:38 +0200

    make_data.py

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # Computational Data Analysis - Case 2
 Exploratory (unsupervised) analysis using a cropped and aligned version of the [UTKFaces](https://susanqq.github.io/UTKFace/) dataset.
 
+## Data
+The main dataset contains 23705 cropped and aligned face images. The dataset is quite large and therefore not well suited for versioning. Therefore the face image data is ignored through the `.gitignore`, but can be downloaded from [here](https://files.dtu.dk/userportal/?v=4.5.2#/shared/public/0aKdY0cinWfDop1p/716774b2-c059-4082-8d00-0297b88838d4).
+<br>
+Unzip the data and place the content in the `data` folder. This includes the `Faces` folder, `filenames.txt`, `labels.csv`, and (if you want) `readme.txt` file.
+
+## Repository Structure
+The structure of this repo follows the [Cookiecutter Data Science Template](https://drivendata.github.io/cookiecutter-data-science/#directory-structure).
+
 ## Authors
 August Semrau Andersen, Sunniva Olsrud Punsvik, Søren Winkel Holm, and William Diedrichsen Marstrand.
---
+++ src/data/make_data.py
@@ -0,0 +1,42 @@
+import os
+import shutil
+from tqdm import tqdm
+import requests as req
+from zipfile import ZipFile 
+
+FACES_FILE_URI1 = 'https://files.dtu.dk/fss/public/link/public/stream/read/data.zip?linkToken=0aKdY0cinWfDop1p&itemName=716774b2-c059-4082-8d00-0297b88838d4'
+
+ZIP_FILE_NAME = 'faces.zip'
+BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+ZIP_FILE_PATH = os.path.join(BASE_PATH, '..', '..', 'data', ZIP_FILE_NAME)
+EXTRACT_PATH = os.path.join(BASE_PATH, '..', '..', 'data')
+
+if __name__ == '__main__':
+    print('Downloading face data...')
+    r = req.get(FACES_FILE_URI1, stream=True)
+
+    print(f'Saving face data to {ZIP_FILE_PATH}')
+    total_size_in_bytes= int(r.headers.get('content-length', 0))
+    block_size = 1024 #1 Kibibyte
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(ZIP_FILE_PATH, 'wb') as faces:
+        for data in r.iter_content(block_size):
+            progress_bar.update(len(data))
+            faces.write(data)
+
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("ERROR, something went wrong")
+
+    print('Unzipping data...')
+    with ZipFile(ZIP_FILE_PATH, 'r') as zip:
+        zip.extractall(EXTRACT_PATH)
+
+
+    for name in os.listdir(os.path.join(EXTRACT_PATH, 'data')):
+        shutil.move(os.path.join(EXTRACT_PATH, 'data', name), os.path.join(EXTRACT_PATH, name))
+
+    os.removedirs(os.path.join(EXTRACT_PATH, 'data'))
+    os.remove(ZIP_FILE_PATH)
+
+    print('Data created sucessfully!')
