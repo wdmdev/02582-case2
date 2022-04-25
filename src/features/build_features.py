723a7bd8bb341ed235ee23908a557d0e8b4ed950
Søren Winkel Holm <swholm@protonmail.com>
2022-04-25 11:49:23 +0200

    Add embedder and classifier flow

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -10,11 +10,23 @@ from serialization import save_features
 BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 RAW_PATH = os.path.join(BASE_PATH, '..', '..', 'data', 'raw')
 
+TRAIN_SPLIT = 0.8
+
 def convert_to_dataframe(path: str=RAW_PATH):
+    # Load simple features
     df = pd.read_csv(os.path.join(path, "labels.csv"), header=None, names=("age", "gender", "race"))
+
+    # Load images
     get_img = lambda row: np.array(Image.open(os.path.join(RAW_PATH, "Faces", f"{row.name}.jpg")))
     tqdm.pandas(desc="Loading image arrays")
     df["image"] = df.progress_apply(get_img, axis=1)
+
+    # Divide into train and test
+    df["train"] = False
+    idx = np.arange(n := len(df))
+    np.random.shuffle(idx)
+    train_idx = idx[:int(TRAIN_SPLIT*n)]
+    df.loc[train_idx, "train"] = True
     return df
 
 if __name__ == "__main__":
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -5,14 +5,43 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '
 import numpy as np
 import pandas as pd
 from skimage import color
-from sklearn.decomposition import MiniBatchDictionaryLearning
+
+from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+from sklearn.base import clone
+from sklearn.metrics import confusion_matrix, classification_report, mean_squared_error, r2_score
+
 
 from src.features.serialization import load_features
 
-def divide_into_agegroups(df: pd.DataFrame, n_groups: int):
-    age_lists = np.array_split(np.arange(df["age"].min(), df["age"].max()+1), n_groups)
-    datasets = [df[[age in age_list for age in df["age"]]] for age_list in age_lists]
-    return datasets
+EMBEDDERS = (
+    PCA(n_components=100),
+)
+
+CLASSIFIERS = (
+    KNeighborsClassifier(n_neighbors=5, n_jobs=-1),
+)
+REGRESSORS = (
+    KNeighborsRegressor(n_neighbors=5, n_jobs=-1),
+)
+
+def run_feature_robustness_test(model, df: pd. DataFrame, e_train: np.ndarray, e_test: np.ndarray, feature: str):
+    print(f"\tTesting {feature} robustness of embeddings using {model}...")
+    y = df[feature].values
+    y_train, y_test = y[df["train"]], y[~df["train"]]
+
+    # Train on embeddings
+    model.fit(e_train, y_train)
+    pred_train, pred_test = model.predict(e_train), model.predict(e_test)
+
+    if feature == "age":
+        # Regression stats
+        print(f"Train MSE, r^2 {mean_squared_error(y_train, pred_train):.2f}, {r2_score(y_train, pred_train):.2f}")
+        print(f"Test MSE, r^2 {mean_squared_error(y_test, pred_test):.2f}, {r2_score(y_test, pred_test):.2f}")
+    else:
+        # Classification stats
+        print("Train conf. mat., report", confusion_matrix(y_train, pred_train), classification_report(y_train, pred_train), sep="\n")
+        print("Test conf. mat., report", confusion_matrix(y_test, pred_test), classification_report(y_test, pred_test), sep="\n")
 
 def build_data_matrix(df: pd.DataFrame):
     X = np.stack(df["image"])
@@ -21,21 +50,21 @@ def build_data_matrix(df: pd.DataFrame):
     X = X.reshape(X.shape[0], -1)
     return X
 
-def train(X: np.ndarray):
-    model = MiniBatchDictionaryLearning(n_components=15, transform_alpha=0.1, alpha=0.1, n_iter=50, batch_size=3)
-    Z = model.fit_transform(X)
-    return Z
-
 def main():
-    N = 10
     df = load_features()
     df = df.sample(1000) # <- memory limitation
-    age_dfs = divide_into_agegroups(df, N)
-    for age_df in age_dfs:
-        print(f"Running for age group {age_df.age.min()}-{age_df.age.max()}...")
-        X = build_data_matrix(age_df)
-        Z = train(X)
-
+    Xtrain, Xtest = build_data_matrix(df[df["train"]]), build_data_matrix(df[~df["train"]])
+    for embedder in EMBEDDERS:
+        print(f"Creating face embeddings using {embedder}...")
+        train_embeddings = embedder.fit_transform(Xtrain)
+        test_embeddings = embedder.transform(Xtest)
+        for classifier in CLASSIFIERS:
+            classifier = clone(classifier) # Make sure there is no leakage between runs
+            for f in "race", "gender":
+                run_feature_robustness_test(classifier, df, train_embeddings, test_embeddings, f)
+        for regressor in REGRESSORS:
+            regressor = clone(regressor)
+            run_feature_robustness_test(regressor, df, train_embeddings, test_embeddings, "age")
 
 if __name__ == "__main__":
     main()
