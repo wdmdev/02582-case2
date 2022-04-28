6b1d93b115b3803cc97bf8e2d6d03d21d3183b85
Søren Winkel Holm <swholm@protonmail.com>
2022-04-28 09:43:40 +0200

    Add noise test

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import argparse
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..'))
 
 import numpy as np
@@ -28,8 +29,11 @@ MODELS = [
     for embedder in EMBEDDERS
 ]
 
+S_AND_P_AMOUNT = 0.5
+
 BASE_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)), '..', '..')
 REPORT_PATH = os.path.join(BASE_PATH, 'reports')
+
 
 def run_feature_robustness_test(model, model_name:str, df: pd. DataFrame, e_train: np.ndarray, e_test: np.ndarray, feature: str):
     print(f"\tTesting {feature} robustness of embeddings using {model}...")
@@ -60,6 +64,41 @@ def run_feature_robustness_test(model, model_name:str, df: pd. DataFrame, e_trai
             report_file.write(msg)
             print(msg)
 
+def gauss(img: np.ndarray) -> np.ndarray:
+    return img + np.random.normal(img.std(), size=img.shape)
+
+def salt_and_pepper(img: np.ndarray) -> np.ndarray:
+    noisy = img.copy()
+    # Salt mode
+    num_salt = np.ceil(S_AND_P_AMOUNT * img.size * 0.5)
+    noisy[np.random.randint(0, img.size - 1, int(num_salt))] = 1
+    # Pepper mode
+    num_pepper = np.ceil(S_AND_P_AMOUNT * img.size * 0.5)
+    noisy[np.random.randint(0, img.size - 1, int(num_pepper))] = 0
+    return noisy
+
+def run_noise_robustness_test(model: Model, Xtest: np.ndarray, e_test: np.ndarray):
+    for noisename, noisefunc in zip(
+            (f"Gaussian", f"Salt and Pepper ({S_AND_P_AMOUNT})"),
+            (gauss, salt_and_pepper)
+        ):
+        preds, truth = np.zeros(Xtest.shape[0]), np.arange(Xtest.shape[0])
+        for i, img in enumerate(Xtest):
+            noisy = noisefunc(img)
+            new_emb = model.embedder.transform(noisy.reshape(1, -1))
+            dists = ((new_emb - e_test)**2).sum(1)
+            # TODO: What about top K-performance?
+            preds[i] = dists.argmin()
+
+        with open(os.path.join(REPORT_PATH, model.name + '.txt'), 'a') as report_file:
+            msg = f'''
+            Test set {noisename} noise prediction accuracy {(preds == truth).mean()*100:.2f}
+            '''
+            report_file.write(msg)
+            print(msg)
+
+
+
 def build_data_matrix(df: pd.DataFrame):
     X = np.stack(df["image"])
     X = X / 255 # <- converts to float
@@ -67,7 +106,7 @@ def build_data_matrix(df: pd.DataFrame):
     X = X.reshape(X.shape[0], -1)
     return X
 
-def main():
+def main(run_feature: bool=True, run_noise: bool=True):
     df = load_features()
     df = df.sample(1000) # <- memory limitation
     Xtrain, Xtest = build_data_matrix(df[df["train"]]), build_data_matrix(df[~df["train"]])
@@ -78,10 +117,17 @@ def main():
         print(f"Creating face embeddings using {model.embedder}...")
         train_embeddings = model.embedder.fit_transform(Xtrain)
         test_embeddings = model.embedder.transform(Xtest)
-        run_feature_robustness_test(model.race_classifier, model.name, df, train_embeddings, test_embeddings, 'race')
-        run_feature_robustness_test(model.gender_classifier, model.name, df, train_embeddings, test_embeddings, 'gender')
-        run_feature_robustness_test(model.age_regressor, model.name, df, train_embeddings, test_embeddings, "age")
+        if run_feature:
+            run_feature_robustness_test(model.race_classifier, model.name, df, train_embeddings, test_embeddings, 'race')
+            run_feature_robustness_test(model.gender_classifier, model.name, df, train_embeddings, test_embeddings, 'gender')
+            run_feature_robustness_test(model.age_regressor, model.name, df, train_embeddings, test_embeddings, "age")
+        if run_noise:
+            run_noise_robustness_test(model, Xtest, test_embeddings)
+
         model.save()
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument("--test", default="all", help="What tests to run")
+    t = parser.parse_args().test
+    main(run_feature=t=="all" or t=="feature", run_noise=t=="all" or t=="noise")
